c6ba5332c0e471da8f7cabf145153e4adfe6f91b
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-08 20:41:25 +0600

    create a dynamic page

---
+++ src/app/product/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DynamicPage = () => {
+    return (
+        <div>
+            <h2>This is a Dynamic Page</h2>
+        </div>
+    );
+};
+
+export default DynamicPage;
