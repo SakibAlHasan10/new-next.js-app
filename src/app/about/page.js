538d675566b7990d6edae2dd92da19868e510eef
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-08 19:03:47 +0600

    add name

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,6 +4,11 @@ const AboutPage = () => {
     return (
         <div>
             <h1>This is about page</h1>
+            <div>
+                <h3>Our team</h3>
+                <p>sakib
+                </p>
+            </div>
         </div>
     );
 };
