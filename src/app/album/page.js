018a927c21ac136f4a8ef739aa4536e0b4908a4a
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-14 19:20:49 +0600

    added extrnal image and internal image

--- src/app/album/page.js
+++ src/app/album/page.js
@@ -1,11 +1,16 @@
 import Image from 'next/image';
 import React from 'react';
-
+import nextImage from '@/assets/nextjs-image.webp'
 const AlbumPage = () => {
     return (
         <div>
             <h2>Album page</h2>
             <Image src="https://mobisoftinfotech.com/resources/wp-content/uploads/2022/04/next-JS-framework.png" 
+            width={500}
+            height={300}
+            alt='Nextjs image'
+            />
+            <Image src={nextImage} 
             width={500}
             height={300}
             alt='Nextjs image'
