a518b4093b5531fdd19902bfcbfd22fdb6b42016
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-16 16:50:05 +0600

    update code

--- src/app/post/page.js
+++ src/app/post/page.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const PostsPage = async() => {
-    const res = await fetch("http://localhost:5000/post")
+    const res = await fetch("http://localhost:5000/post",{cache:"force-cache"})
     const posts = await res.json()
     // console.log(posts)
     return (
         <div>
             <h2>This is post page</h2>
-            <h4>Total post: {posts?.length}</h4>
+            <h4>Total post: {posts?.length} </h4>
+            {
+                posts?.map(post=><div className='text-center w-8/12' key={post.id}>
+                    <h3>{post.name}</h3>
+                    <p>{post.description}</p>
+                </div>)
+            }
         </div>
     );
 };
